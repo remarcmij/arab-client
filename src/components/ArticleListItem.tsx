1a02d60e7a294a0edc08daf5175571067700af6f
Jim Cramer <remarcmij@gmail.com>
2019-02-28 22:38:49 +0100

    Add icon avatar to publication and article lists

--- src/components/ArticleListItem.tsx
+++ src/components/ArticleListItem.tsx
@@ -1,20 +1,53 @@
+import Avatar from '@material-ui/core/Avatar'
+import green from '@material-ui/core/colors/green'
+import pink from '@material-ui/core/colors/pink'
 import ListItem from '@material-ui/core/ListItem'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import ListItemText from '@material-ui/core/ListItemText'
+import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles'
+import Code from '@material-ui/icons/Code'
+import Notes from '@material-ui/icons/Notes'
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 import Types from 'Types'
 
-type Props = {
+const styles = () =>
+  createStyles({
+    pinkAvatar: {
+      color: '#fff',
+      backgroundColor: pink[500],
+    },
+    greenAvatar: {
+      color: '#fff',
+      backgroundColor: green[500],
+    },
+  })
+
+interface Props extends WithStyles<typeof styles> {
   publication: Types.AppDocument
 }
 
 const ArticleListItem: React.FC<Props> = props => {
-  const { publication, article, title, subtitle } = props.publication
+  const { classes } = props
+  const { publication, article, title, subtitle, kind } = props.publication
 
   const ItemLink = (p: {}) => <Link to={`/content/${publication}/${article}`} {...p} />
 
   return (
     <ListItem component={ItemLink} button={true}>
+      {kind === 'table' ? (
+        <ListItemAvatar className={classes.pinkAvatar}>
+          <Avatar>
+            <Code />
+          </Avatar>
+        </ListItemAvatar>
+      ) : (
+        <ListItemAvatar className={classes.greenAvatar}>
+          <Avatar>
+            <Notes color="inherit" />
+          </Avatar>
+        </ListItemAvatar>
+      )}
       <ListItemText
         primary={title}
         secondary={<span dangerouslySetInnerHTML={{ __html: subtitle || '' }} />}
@@ -23,4 +56,4 @@ const ArticleListItem: React.FC<Props> = props => {
   )
 }
 
-export default ArticleListItem
+export default withStyles(styles)(ArticleListItem)
--- src/components/PublicationListItem.tsx
+++ src/components/PublicationListItem.tsx
@@ -1,20 +1,39 @@
+import Avatar from '@material-ui/core/Avatar'
+import blue from '@material-ui/core/colors/blue'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 import Types from 'Types'
+import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import FolderOpen from '@material-ui/icons/FolderOpen'
 
-type Props = {
+const styles = () =>
+  createStyles({
+    blueAvatar: {
+      color: '#fff',
+      backgroundColor: blue[500],
+    },
+  })
+
+interface Props extends WithStyles<typeof styles> {
   publication: Types.AppDocument
 }
 
 const PublicationListItem: React.FC<Props> = props => {
+  const { classes } = props
   const { publication, title, subtitle } = props.publication
 
   const ItemLink = (p: {}) => <Link to={`/content/${publication}/index`} {...p} />
 
   return (
     <ListItem component={ItemLink} button={true}>
+      <ListItemAvatar className={classes.blueAvatar}>
+        <Avatar>
+          <FolderOpen />
+        </Avatar>
+      </ListItemAvatar>
       <ListItemText
         primary={title}
         secondary={<span dangerouslySetInnerHTML={{ __html: subtitle || '' }} />}
@@ -23,4 +42,4 @@ const PublicationListItem: React.FC<Props> = props => {
   )
 }
 
-export default PublicationListItem
+export default withStyles(styles)(PublicationListItem)
