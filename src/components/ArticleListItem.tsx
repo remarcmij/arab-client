b2a35a2774845e842c78d57d520e98f614c2896a
Jim Cramer <remarcmij@gmail.com>
2019-05-02 11:15:41 +0200

    Prepare for next branch

--- src/components/ArticleListItem.tsx
+++ src/components/ArticleListItem.tsx
@@ -24,7 +24,7 @@ const styles = () =>
   });
 
 interface OwnProps {
-  publication: Types.AppDocument;
+  readonly publication: Types.AppDocument;
 }
 
 type Props = OwnProps & WithStyles<typeof styles>;
