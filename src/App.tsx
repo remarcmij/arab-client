c047aa62c7bf8ee837b6822ba042e3b7a0a78ac8
Jim Cramer <remarcmij@gmail.com>
2019-12-08 11:18:28 +0100

    Replace CSS media query with hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,31 @@
+import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import React, { Suspense, useEffect } from 'react';
 import { useCookies } from 'react-cookie';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Routes from './routes/Routes';
 import { loadUserThunk } from './features/auth/actions';
 import SettingsDialog from './features/settings/components/SettingsDialog';
 import NavBar from './layout/components/NavBar';
 import SnackbarContainer from './layout/components/SnackbarContainer';
 import Spinner from './layout/components/Spinner';
 import Welcome from './layout/components/Welcome';
+import Routes from './routes/Routes';
 import store from './store';
 import { storeToken } from './utils/token';
 
 // paddingTop emulates the toolbar's minHeight from the default theme
 const useStyles = makeStyles({
-  root: {
-    flexGrow: 1,
-    paddingTop: 56,
-    '@media (min-width:600px)': {
-      paddingTop: 64,
-    },
+  container: {
+    padding: 0,
   },
 });
 
 const App: React.FC<{}> = () => {
   const [cookies, , removeCookie] = useCookies();
+  const hasMinWidth = useMediaQuery('(min-width:600px)');
   const classes = useStyles();
   const { token } = cookies;
   if (token) {
@@ -42,17 +41,17 @@ const App: React.FC<{}> = () => {
     <Suspense fallback={<Spinner />}>
       <Provider store={store}>
         <Router>
-          <div className={classes.root}>
+          <Box flexGrow={1} pt={hasMinWidth ? 8 : 7}>
             <NavBar />
             <SnackbarContainer />
             <SettingsDialog />
-            <Container maxWidth="md">
+            <Container maxWidth="md" classes={{ root: classes.container }}>
               <Switch>
                 <Route exact={true} path="/welcome" component={Welcome} />
                 <Route component={Routes} />
               </Switch>
             </Container>
-          </div>
+          </Box>
         </Router>
       </Provider>
     </Suspense>
