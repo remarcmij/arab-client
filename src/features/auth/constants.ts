f53d695e00ca08a3b9f6d01cf4ef4d3af33e9e87
Jim Cramer <remarcmij@gmail.com>
2019-12-10 22:59:48 +0100

    Remove obsoleted constants file

--- src/features/auth/constants.ts
+++
@@ -1,9 +0,0 @@
-export const AUTH_ERROR = '@auth/AUTH_ERROR';
-export const LOGIN_FAIL = '@auth/LOGIN_FAIL';
-export const LOGIN_SUCCESS = '@auth/LOGIN_SUCCESS';
-export const LOGOUT = '@auth/LOGOUT';
-export const OPEN_SETTINGS = '@auth/OPEN_SETTINGS';
-export const REGISTER_FAIL = '@auth/REGISTER_FAIL';
-export const REGISTER_SUCCESS = '@auth/REGISTER_SUCCESS';
-export const USER_LOADED = '@auth/USER_LOADED';
-export const PASSWORD_RESET = '@auth/PASSWORD_RESET';
